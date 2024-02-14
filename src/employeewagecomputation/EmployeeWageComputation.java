2e70619a3202442650d2aac19980638ffde8129b
ajaypatel0 <krishnapc700@gmail.com>
2024-02-15 00:12:18 +0530

    [MODIFIED] : Ajay - UC-1 completed for Employee AAttendance

--- src/employeewagecomputation/EmployeeWageComputation.java
+++ src/employeewagecomputation/EmployeeWageComputation.java
@@ -1,10 +1,23 @@
 package employeewagecomputation;
 
+import java.util.Random;
+
 public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int attendance;
+		
 		System.out.println("Welcome to Employee wage computation");
+		
+		Random random=new Random();
+		attendance = random.nextInt(10) % 2 ;
+		if(attendance==0) {
+			System.out.println("Employee is absent");
+		}
+		else {
+			System.out.println("Employee is present");
+		}
 	}
 
 }
