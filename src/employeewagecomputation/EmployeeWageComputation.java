54a5f8a0b5234b3b638a18d1da74f53bfac4452f
ajaypatel0 <krishnapc700@gmail.com>
2024-02-15 00:49:12 +0530

    [MODIFIED] : Ajay - UC-3 Completed for check employee type

--- src/employeewagecomputation/EmployeeWageComputation.java
+++ src/employeewagecomputation/EmployeeWageComputation.java
@@ -7,20 +7,34 @@ public class EmployeeWageComputation {
 	public static void main(String[] args) {
 		
 		int attendance;
+		int employeeType;
 		int dailyWage;
 		final int WAGE_PER_HR = 20;
-		final int FULL_DAY_HR = 8;
+		final int PART_TIME_HR = 4;
+		final int FULL_TIME_HR = 8;
+		
 		System.out.println("Welcome to Employee wage computation");
 		
 		Random random=new Random();
 		attendance = random.nextInt(10) % 2 ;
+		
 		if(attendance==0) {
 			System.out.println("Employee is absent");
 		}
 		else {
-		
+			
 			System.out.println("Employee is present");
-			dailyWage = FULL_DAY_HR * WAGE_PER_HR; 
+			
+			employeeType = random.nextInt(10) % 2;
+			if(employeeType==0) {
+				System.out.println("Employee is part timer");
+				dailyWage = WAGE_PER_HR * PART_TIME_HR;
+			}
+			else {
+				System.out.println("Employee is full timer");
+				dailyWage = WAGE_PER_HR * FULL_TIME_HR;
+			}
+			
 			System.out.println("Employee Wage = "+dailyWage);
 		}
 	}
