bb17b0cff5957495938b735228a35f522234518d
ajaypatel0 <krishnapc700@gmail.com>
2024-02-15 00:28:44 +0530

    [MODIFIED] : Ajay - UC-3 Completed for Calculate daily wage of a employee

--- src/employeewagecomputation/EmployeeWageComputation.java
+++ src/employeewagecomputation/EmployeeWageComputation.java
@@ -7,7 +7,9 @@ public class EmployeeWageComputation {
 	public static void main(String[] args) {
 		
 		int attendance;
-		
+		int dailyWage;
+		final int WAGE_PER_HR = 20;
+		final int FULL_DAY_HR = 8;
 		System.out.println("Welcome to Employee wage computation");
 		
 		Random random=new Random();
@@ -16,7 +18,10 @@ public class EmployeeWageComputation {
 			System.out.println("Employee is absent");
 		}
 		else {
+		
 			System.out.println("Employee is present");
+			dailyWage = FULL_DAY_HR * WAGE_PER_HR; 
+			System.out.println("Employee Wage = "+dailyWage);
 		}
 	}
 
