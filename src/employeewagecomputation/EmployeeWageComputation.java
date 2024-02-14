54115f244cbf15f388c8a034b93a15e68eec8308
ajaypatel0 <krishnapc700@gmail.com>
2024-02-14 23:58:12 +0530

    [ADDED] : Ajay - New project started Employee wage computation

---
+++ src/employeewagecomputation/EmployeeWageComputation.java
@@ -0,0 +1,10 @@
+package employeewagecomputation;
+
+public class EmployeeWageComputation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Employee wage computation");
+	}
+
+}
